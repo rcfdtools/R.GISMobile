847609c4a7dbf5b107a700fb99a23089805c1f5c
r.cfdtools <r.cfdtools@gmail.com>
2026-08-23 17:04:24 -0500

    General update.

--- file/src/igac_export_county.py
+++ file/src/igac_export_county.py
@@ -9,7 +9,7 @@ import processing
 # Export layers
 layer = iface.activeLayer()
 layer_suffix = '_Urban' # ● Suffix for label each exported layer, e.g., U_TERRENO correspond to 'Urban'
-index_field = 'MUNICIPIO' # ● Index field in the selected layer
+index_field = 'CountyIDXXXX' # ● Index field in the selected layer
 print_explicit = False # ● Show explicit running in console
 run_complete = True # ● Run for each index_field value. Use False if you want to get the unique value list
 load_layer_in_map = False # ● Load each exported layer into the current project map
