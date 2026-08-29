e11f2447d0a709b376f5126c30b36971d34eaa25
r.cfdtools <r.cfdtools@gmail.com>
2026-08-29 11:17:54 -0500

    General update.

---
+++ file/src/county_readme.py
@@ -0,0 +1,62 @@
+# https://github.com/rcfdtools
+# -*- coding: UTF-8 -*-
+# Markdown report of individual county layers in /shp
+
+
+# Libraries
+from pathlib import Path
+
+from pandas.core.interchange.from_dataframe import primitive_column_to_ndarray
+from simpledbf import Dbf5
+import tabulate
+import pandas as pd
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+# Function for print and show results in a log file
+def print_log(file_log, txt_print, on_screen=False, center_div=False):
+    # div50 is use for show 2 plots in the same line
+    if on_screen:
+        print(txt_print)
+    if center_div:
+        file_log.write('\n<div align="center">\n' + '\n')
+    file_log.write(txt_print)
+    if center_div:
+        file_log.write('\n\n</div>\n' + '\n')
+
+# Processing
+url_file = 'https://github.com/rcfdtools/R.GISMobile/blob/main/file/shp/'
+dir_path = Path('../shp')
+zip_files = [file.name for file in dir_path.glob('*.zip')]
+file_log_name = f'../shp/Readme.md'  # Markdown file log
+file_log = open(file_log_name, 'w+', encoding='utf-8')  # w+ create the file if it doesn't exist
+# County list
+dbf_county = Dbf5('../shp/ColombiaCounty4326.dbf', codec='cp1252')
+df_county = pd.DataFrame(dbf_county.to_dataframe())
+df_county = df_county[['DeCodigo', 'DeNombre', 'MpCodigo', 'MpNombre', 'MpNorma', 'Latitude', 'Longitude']]
+df_county = df_county.sort_values(by=['DeCodigo', 'DeNombre', 'MpCodigo', 'MpNombre'])
+df_county.drop(df_county[df_county['MpCodigo'] == '00000'].index, inplace=True)
+# State list
+df_state = df_county['DeNombre'].unique()
+print_log(file_log, f'<div align="center"><img alt="rcfdtools" src="../graph/R.GISMobile.svg" width="300px"></div>\n\n')
+print_log(file_log, f'# 🌎GISMobile: Layers por Municipio - Colombia Suramérica')
+for state in df_state:
+    print_log(file_log, f'\n\n\n# {state}')
+    df_county_filter = df_county[df_county['DeNombre'] == state]
+    df_county_unique = df_county_filter['MpCodigo'].unique()
+    for county in df_county_unique:
+        #print_log(file_log, county)
+        df_county_info = df_county[df_county['MpCodigo'] == county]
+        state_name = df_county_info['DeNombre'].values[0]
+        county_name = df_county_info['MpNombre'].values[0]
+        print_log(file_log,f'\n\n**{county_name}** - {county}: ')
+        #zip_files_filter = [item for item in zip_files if group_id in item]
+        zip_files_filter = [item for item in zip_files if item.startswith(county)]
+        if len(zip_files_filter) > 0:
+            for file in zip_files_filter:
+                print_log(file_log,f'[• {file}]({url_file}{file}) ')
+        else:
+            print_log(file_log,'Layers not found')
+
