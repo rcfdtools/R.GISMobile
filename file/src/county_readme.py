c0f24a324a37068e011578e1bd404823f301df68
r.cfdtools <r.cfdtools@gmail.com>
2026-08-29 17:28:54 -0500

    General update.

--- file/src/county_readme.py
+++ file/src/county_readme.py
@@ -1,7 +1,7 @@
 # https://github.com/rcfdtools
 # -*- coding: UTF-8 -*-
 # Markdown report of individual county layers in /shp
-
+from logging import NullHandler
 
 # Libraries
 import functions as funcs
@@ -35,7 +35,7 @@ df_state = df_county['DeCodigo'].unique()
 funcs.print_log(file_log, f'<div align="center"><img alt="rcfdtools" src="../graph/R.GISMobile.svg" width="250px"></div>\n\n')
 funcs.print_log(file_log, f'# {dictionary.dicts['study_name']} \n{dictionary.dicts['keywords']}\n\n{dictionary.dicts['study_desc']}\n\n', on_screen=print_on_screen)
 for state in df_state:
-    print_dataframe = pd.DataFrame(columns=['CountyID', 'CountyName', 'CountyFiles'])
+    print_dataframe = pd.DataFrame(columns=['CountyID', 'CountyName', 'CountyRule', 'CountyFiles'])
     df_state_info = df_county[df_county['DeCodigo'] == state]
     state_name = df_state_info['DeNombre'].values[0]
     df_county_filter = df_county[df_county['DeCodigo'] == state]
@@ -51,7 +51,9 @@ for state in df_state:
     df_county_unique = df_county_filter['MpCodigo'].unique()
     for county in df_county_unique:
         df_county_info = df_county[df_county['MpCodigo'] == county]
+        df_county_info['MpNorma'] = df_county_info['MpNorma'].fillna('')
         county_name = df_county_info['MpNombre'].values[0]
+        CountyRule = df_county_info['MpNorma'].values[0]
         zip_files_filter = [item for item in zip_files if item.startswith(county)]
         files_txt = ''
         if len(zip_files_filter) > 0:
@@ -59,7 +61,7 @@ for state in df_state:
                 files_txt += f'[{file}]({url_file}{file})<br/>'
         else:
             files_txt = 'Not found'
-        print_dataframe.loc[len(print_dataframe)] = [county, county_name, files_txt]
+        print_dataframe.loc[len(print_dataframe)] = [county, county_name, CountyRule, files_txt]
     funcs.print_log(file_log, print_dataframe.to_markdown(index=False), center_div=True)
 funcs.print_log(file_log, f'\n#\n\n<div align="center"><img alt="rcfdtools" src="../graph/qr-code-shp.png" width="250px"><br><sub>Share this research</sub></div><br>', on_screen = print_on_screen)
 funcs.print_log(file_log, f'\n\n<sub>{dictionary.dicts['disclaimer']}</sub>', on_screen = print_on_screen)
