e94cda0ef910efe47e85167beffe088340e2b1a0
r.cfdtools <r.cfdtools@gmail.com>
2026-08-29 07:55:29 -0500

    General update.

--- file/src/county_zip_grouped.py
+++ file/src/county_zip_grouped.py
@@ -6,8 +6,8 @@ import glob
 import zipfile
 from pathlib import Path
 
-directory = '../temp/'
-run_complete = True # ● Run for each county founded. Use False if you want to get the unique value list
+directory = '../shp/'
+run_complete = False # ● Run for each county founded. Use False if you want to get the unique value list
 version_info = f'# Dataset Information\n\n* More information in https://github.com/rcfdtools/R.GISMobile/blob/main/file/shp/Readme.md'
 files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 exclude_file_type = ['.zip', '.xml', '.cpg', '.sbn', '.sbx', '.qix', '.qmd', '.ovr']
--- file/src/download_esri_rest_layer_with_limited_records.py
+++ file/src/download_esri_rest_layer_with_limited_records.py
@@ -1,4 +1,4 @@
-import json
+1import json
 import requests
 
 #service_url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3/query"
