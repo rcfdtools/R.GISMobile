0c3ee1e9fb8b939e55ff40343c8e6457166ffa86
r.cfdtools <r.cfdtools@gmail.com>
2026-09-02 16:44:03 -0500

    General update.

--- file/src/functions.py
+++ file/src/functions.py
@@ -22,36 +22,58 @@ def print_log(file_log, txt_print, on_screen=False, center_div=False):
         file_log.write('\n\n</div>\n' + '\n')
 
 # Location map with GeoPandas (single)
-def location_map(point_latitude, point_longitude, point_name, state_filter, county_label_on = False, show_marker = True, horizontal_size = 6, vertical_size = 6):
+def location_map(point_latitude, point_longitude, point_name, state_filter = 'All', county_label_on = False, show_marker = True, horizontal_size = 6, vertical_size = 6, plot_state = False, county_filter = 'All', plot_only_shape = False):
+    county_linewidth = 0.25
+    state_linewidth = 1.25
+    fontsize = 10
+    xytext = (6, 6)
+    if state_filter == 'All' and county_filter == 'All': state_linewidth = 1.0
     if state_filter == 'All':
         state_shapefile = gpd.read_file('../shp/ColombiaState4326.shp')
         county_shapefile = gpd.read_file('../shp/ColombiaCounty4326.shp')
     else:
-        # Under construction
         state_shapefile = gpd.read_file('../shp/ColombiaState4326.shp', where=f"DeCodigo = '{state_filter}'")
         county_shapefile = gpd.read_file('../shp/ColombiaCounty4326.shp', where=f"DeCodigo = '{state_filter}'")
+        #county_shapefile = gpd.read_file('../shp/ColombiaCounty4326.shp', where=f"MpCodigo = '{county_filter}'")
+    if county_filter != 'All':
+        county_shapefile = gpd.read_file('../shp/ColombiaCounty4326.shp', where=f"MpCodigo = '{county_filter}'")
     point_location = Point(point_longitude, point_latitude)
     point_gdf = gpd.GeoDataFrame(geometry=[point_location], crs=state_shapefile.crs)
     fig, ax = plt.subplots(figsize=(horizontal_size, vertical_size))  # Adjust figure size as needed
-    if county_label_on:
-        state_shapefile.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=1.25, legend=True, legend_kwds={'fontsize': 'small'}, label='DeCodigo')
+    if plot_only_shape:
+        county_linewidth = 2 # Applied when you print only the county limit
+        plt.margins(0)
+        ax.margins(0.025)
+        ax.axis('off')
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        fontsize = 18
+        xytext = (-80, 0)
+    if plot_state:
+        county_shapefile.boundary.plot(ax=ax, edgecolor='black', linewidth=county_linewidth)  # , label='AH'
+        if county_label_on:
+            state_shapefile.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=state_linewidth, legend=True, legend_kwds={'fontsize': 'small'}, label='DeCodigo')
+        else:
+            state_shapefile.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=state_linewidth, legend=True, legend_kwds={'fontsize': 'small'})
     else:
-        state_shapefile.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=1.25, legend=True, legend_kwds={'fontsize': 'small'})
+        # county_shapefile.boundary.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=county_linewidth) # , label='AH'
+        county_shapefile.plot(ax=ax, color='lightgray', edgecolor='black', linewidth=county_linewidth)  # , label='AH'
     #state_shapefile.plot(ax=ax, column='DeCodigo', cmap='Greens', edgecolor='black', linewidth=0.75, legend=True, legend_kwds={'fontsize': 'small'}) # , label='AH'
-    county_shapefile.boundary.plot(ax=ax, edgecolor='black', linewidth=0.25) # , label='AH'
+    #ax.legend(loc='lower left')
+    if not plot_only_shape:
+        ax.set_title("Map Location")
+        plt.xlabel("Longitude°")
+        plt.ylabel("Latitude°")
+        ax.tick_params(axis='both', labelsize=9)
+    else:
+        plt.axis('off')
     if show_marker:
         point_gdf.plot(ax=ax, marker='o', color=accent_color, markersize=40, legend=False)  # color='black', 'marker' and 'markersize' customize the point
-    #ax.legend(loc='lower left')
-    ax.set_title("Map Location")
-    plt.xlabel("Longitude°")
-    plt.ylabel("Latitude°")
-    ax.tick_params(axis='both', labelsize=9)
     ax.annotate(
         text= point_name,
         xy=(point_longitude, point_latitude),
-        xytext=(6, 6),  # Offset the text slightly (e.g., 5 points right, 5 points up)
+        xytext=xytext,  # Offset the text slightly (e.g., 5 points right, 5 points up)
         textcoords="offset points",
-        fontsize=10,
+        fontsize=fontsize,
         color='white',
         bbox=dict(boxstyle='round', facecolor=accent_color, alpha=0.9, pad=0.25)
     )
--- file/src/minimap.py
+++ file/src/minimap.py
@@ -18,9 +18,9 @@ minimap_path = '../gis/MiniMAP/'
 dpi_resolution = 180
 run_colombia_state = True
 run_colombia_county_national_point = False
+run_colombia_county_simple = False
 
 # Colombia State MiniMAP
-# Source:
 if run_colombia_state:
     dpi_resolution = 180
     country_code = '57'
@@ -38,18 +38,17 @@ if run_colombia_state:
         latitude = df_state_info['Latitude'].values[0]
         longitude = df_state_info['Longitude'].values[0]
         print(figure)
-        location_map_plot = funcs.location_map(point_latitude = latitude, point_longitude = longitude, point_name = f'{minimap_name} - {name.upper()}', state_filter = minimap_name, county_label_on = True, show_marker = False, horizontal_size = 6, vertical_size = 6)
+        location_map_plot = funcs.location_map(point_latitude = latitude, point_longitude = longitude, point_name = f'{minimap_name} - {name.upper()}', state_filter = minimap_name, county_label_on = True, show_marker = False, horizontal_size = 6, vertical_size = 6, plot_state=True, county_filter = 'All', plot_only_shape = False)
         location_map_plot.savefig(figure, dpi=dpi_resolution)
         plt.close()
 
 # Colombia County National Point Location
-# Source:
 if run_colombia_county_national_point:
     dpi_resolution = 180
     country_code = '57'
     dbf_layer = Dbf5('../shp/ColombiaCounty4326.dbf', codec='cp1252')
     df_layer = pd.DataFrame(dbf_layer.to_dataframe())
-    df_layer = df_layer[['MpCodigo', 'MpNombre', 'MpNorma', 'Latitude', 'Longitude']]
+    df_layer = df_layer[['MpCodigo', 'MpNombre', 'MpNorma', 'Latitude', 'Longitude', 'LatCentr', 'LonCentr']]
     df_layer = df_layer.sort_values(by=['MpCodigo', 'MpNombre'])
     df_layer.drop(df_layer[df_layer['MpCodigo'] == '00000'].index, inplace=True)
     db_layer_list = df_layer['MpCodigo'].unique()
@@ -58,9 +57,31 @@ if run_colombia_county_national_point:
         figure = f'{minimap_path}{country_code}_{minimap_name}_LocationMapCountry.png'
         df_state_info = df_layer[df_layer['MpCodigo'] == minimap_name]
         name = df_state_info['MpNombre'].values[0]
-        latitude = df_state_info['Latitude'].values[0]
-        longitude = df_state_info['Longitude'].values[0]
+        latitude = df_state_info['LatCentr'].values[0]
+        longitude = df_state_info['LonCentr'].values[0]
         print(figure)
-        location_map_plot = funcs.location_map(point_latitude = latitude, point_longitude = longitude, point_name = f'{minimap_name} - {name.upper()}', state_filter = 'All', county_label_on = True, show_marker = True, horizontal_size = 6, vertical_size = 6)
+        location_map_plot = funcs.location_map(point_latitude = latitude, point_longitude = longitude, point_name = f'{minimap_name} - {name.upper()}', state_filter = 'All', county_label_on = True, show_marker = True, horizontal_size = 6, vertical_size = 6, plot_state=True, county_filter = 'All', plot_only_shape = False)
+        location_map_plot.savefig(figure, dpi=dpi_resolution)
+        plt.close()
+
+# Colombia County simple polygon: LocationMapCountySimple.png
+if run_colombia_county_simple:
+    dpi_resolution = 100
+    country_code = '57'
+    dbf_layer = Dbf5('../shp/ColombiaCounty4326.dbf', codec='cp1252')
+    df_layer = pd.DataFrame(dbf_layer.to_dataframe())
+    df_layer = df_layer[['MpCodigo', 'MpNombre', 'MpNorma', 'Latitude', 'Longitude', 'LatCentr', 'LonCentr']]
+    df_layer = df_layer.sort_values(by=['MpCodigo', 'MpNombre'])
+    df_layer.drop(df_layer[df_layer['MpCodigo'] == '00000'].index, inplace=True)
+    db_layer_list = df_layer['MpCodigo'].unique()
+    print(f'Colombia county: {db_layer_list}\n')
+    for minimap_name in db_layer_list:
+        figure = f'{minimap_path}{country_code}_{minimap_name}_LocationMapCountySimple.png'
+        df_state_info = df_layer[df_layer['MpCodigo'] == minimap_name]
+        name = df_state_info['MpNombre'].values[0]
+        latitude = df_state_info['LatCentr'].values[0]
+        longitude = df_state_info['LonCentr'].values[0]
+        print(figure)
+        location_map_plot = funcs.location_map(point_latitude = latitude, point_longitude = longitude, point_name = f'{minimap_name} - {name.upper()}', state_filter = 'All', county_label_on = False, show_marker = False, horizontal_size = 6, vertical_size = 6, plot_state=False, county_filter = minimap_name, plot_only_shape = True)
         location_map_plot.savefig(figure, dpi=dpi_resolution)
         plt.close()
