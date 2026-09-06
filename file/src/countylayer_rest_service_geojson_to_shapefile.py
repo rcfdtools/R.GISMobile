aefbed6b22ebd948a31af73264cf84a36dbb3198
r.cfdtools <r.cfdtools@gmail.com>
2026-09-06 09:19:47 -0500

    General update.

--- file/src/countylayer_rest_service_geojson_to_shapefile.py
+++ file/src/countylayer_rest_service_geojson_to_shapefile.py
@@ -14,17 +14,27 @@
 # esri2geojson https://geodatos.antioquia.gov.co/server/rest/services/Catastro/Visor_Geo/FeatureServer/7 AntioquiaContruccionUrbano202608.geojson
 # esri2geojson https://services2.arcgis.com/RVvWzU3lgJISqdke/ArcGIS/rest/services/Base_Catastral_Publica_del_Gestor_IGAC_06_2026/FeatureServer/17 igac_202606_registro1.geojson
 # esri2geojson https://services2.arcgis.com/RVvWzU3lgJISqdke/ArcGIS/rest/services/Base_Catastral_Publica_del_Gestor_IGAC_06_2026/FeatureServer/18 igac_202606_registro2.geojson
+# esri2geojson https://www.medellin.gov.co/servidormapas/rest/services/ServiciosCatastro/Base_Catastral/MapServer/3 MedellinLote202608.geojson
+# esri2geojson https://www.medellin.gov.co/servidormapas/rest/services/ServiciosCatastro/Base_Catastral/MapServer/5 MedellinConstruccion202608.geojson
+# esri2geojson https://portalidem.metropol.gov.co/server/rest/services/Barbosa_Catastro/MapServer/7 05079BarbosaPredioUbano202608.geojson
+# esri2geojson https://portalidem.metropol.gov.co/server/rest/services/Barbosa_Catastro/MapServer/8 05079BarbosaPredioRural202608.geojson
+# esri2geojson https://portalidem.metropol.gov.co/server/rest/services/Barbosa_Catastro/MapServer/5 05079BarbosaConstruccionUbana202608.geojson
+# esri2geojson https://portalidem.metropol.gov.co/server/rest/services/Barbosa_Catastro/MapServer/6 05079BarbosaConstruccionRural202608.geojson
 
 # Convert geojson to shapefile (GeoPandas)
 import geopandas as gpd
 
-file_name = 'AntioquiaBarrios202608'
+file_name_input = '05631SabanetaPredioUrbano202608'
+#file_name_output = '05631_Rural_Building_202608'
+#file_name_output = '05631_Urban_Building_202608'
+#file_name_output = '05631_Rural_Lot_202608'
+file_name_output = '05631_Urban_Lot_202608'
 
 # 1. Read the GeoJSON file into a GeoDataFrame
-gdf = gpd.read_file(f'../geojson/{file_name}.geojson')
+gdf = gpd.read_file(f'../geojson/{file_name_input}.geojson')
 
 # 2. Write the GeoDataFrame out as an ESRI Shapefile
-gdf.to_file(f'../temp/{file_name}.shp', driver="ESRI Shapefile")
+gdf.to_file(f'../temp/{file_name_output}.shp', driver="ESRI Shapefile")
 
 
 # Batch Converting Multiple Files (GeoPandas)
