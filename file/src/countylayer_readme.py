69a5824a698cdc70178865ac0cf401494d092509
r.cfdtools <r.cfdtools@gmail.com>
2026-09-09 13:41:55 -0500

    General update.

--- file/src/countylayer_readme.py
+++ file/src/countylayer_readme.py
@@ -102,7 +102,7 @@ for state in df_state:
                 files_txt += f'[{file}]({url_file}{file})<br/>'
         else:
             files_txt = 'Not found'
-        print_dataframe.loc[len(print_dataframe)] = [county_minimap, county_ppsd_link, cadastre_manager, county_name, files_txt]
+        print_dataframe.loc[len(print_dataframe)] = [county_minimap, county_ppsd_link, county_name, cadastre_manager, files_txt]
     funcs.print_log(file_log, print_dataframe.to_markdown(index=False), center_div=True)
     funcs.print_log(file_log, f'\n#\n\n<div align="center"><img alt="rcfdtools" src="../../graph/qr-code-shp.png" width="250px"><br><sub>Share this research</sub></div><br>', on_screen = print_on_screen)
     funcs.print_log(file_log, f'\n\n<sub>{dictionary.dicts['disclaimer']}</sub>', on_screen = print_on_screen)
