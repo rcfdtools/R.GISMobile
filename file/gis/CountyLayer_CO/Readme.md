9fc44ad84ec3e3740a2eb72b74f54b799260f734
r.cfdtools <r.cfdtools@gmail.com>
2026-09-10 08:25:51 -0500

    General update.

--- file/gis/CountyLayer_CO/Readme.md
+++ file/gis/CountyLayer_CO/Readme.md
@@ -12,7 +12,7 @@ County GIS layers are individual digital map datasets stacked together in a Geog
 </div>
 
 
-## Colombian States (es: Departamentos)
+## Colombia South America States (es: Departamentos)
 
 In Colombia, "Departamentos" (Departments) are the main regional administrative divisions that split the country into 33 large political areas, one special Capital District, which is Bogotá D.C. They are the top level of regional government below the national government. You can think of them like states in the United States or provinces in other nations.
 
--- file/src/countylayer_readme.py
+++ file/src/countylayer_readme.py
@@ -60,7 +60,7 @@ df_state = df_county['DeCodigo'].unique()
 funcs.print_log(file_log, f'<div align="center"><img alt="rcfdtools" src="../../graph/R.GISMobile.svg" width="250px"></div>\n\n')
 funcs.print_log(file_log, f'# _{dictionary.dicts['study_name']}_ \n{dictionary.dicts['keywords']}\n\n{dictionary.dicts['study_desc']} Check the general [Datasource & ChangeLog](Readme_Datasource.md) Readme file.\n', on_screen=print_on_screen)
 funcs.print_log(file_log, f'<img alt="rcfdtools" src="{minimap_link}{country_code}_MiniMapCountry.png" width="600px"></img>', center_div=True, on_screen=print_on_screen)
-funcs.print_log(file_log, f'\n## Colombian States (es: Departamentos)\n\n{dictionary.dicts['state']}\n', on_screen=print_on_screen)
+funcs.print_log(file_log, f'\n## Colombia South America States (es: Departamentos)\n\n{dictionary.dicts['state']}\n', on_screen=print_on_screen)
 for state in df_state:
     df_state_info = df_county[df_county['DeCodigo'] == state]
     state_name = df_state_info['DeNombre'].values[0]
