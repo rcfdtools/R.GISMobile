da15a16f0bdc0a08349567d6e68ede8281cb8406
r.cfdtools <r.cfdtools@gmail.com>
2026-09-10 08:26:34 -0500

    General update.

--- file/src/countylayer_readme.py
+++ file/src/countylayer_readme.py
@@ -60,14 +60,14 @@ df_state = df_county['DeCodigo'].unique()
 funcs.print_log(file_log, f'<div align="center"><img alt="rcfdtools" src="../../graph/R.GISMobile.svg" width="250px"></div>\n\n')
 funcs.print_log(file_log, f'# _{dictionary.dicts['study_name']}_ \n{dictionary.dicts['keywords']}\n\n{dictionary.dicts['study_desc']} Check the general [Datasource & ChangeLog](Readme_Datasource.md) Readme file.\n', on_screen=print_on_screen)
 funcs.print_log(file_log, f'<img alt="rcfdtools" src="{minimap_link}{country_code}_MiniMapCountry.png" width="600px"></img>', center_div=True, on_screen=print_on_screen)
-funcs.print_log(file_log, f'\n## Colombia South America States (es: Departamentos)\n\n{dictionary.dicts['state']}\n', on_screen=print_on_screen)
+funcs.print_log(file_log, f'\n## 1. Colombia South America States (es: Departamentos)\n\n{dictionary.dicts['state']}\n', on_screen=print_on_screen)
 for state in df_state:
     df_state_info = df_county[df_county['DeCodigo'] == state]
     state_name = df_state_info['DeNombre'].values[0]
     df_county_filter = df_county[df_county['DeCodigo'] == state]
     funcs.print_log(file_log, f'\n* [{state} - {state_name}]({state}.md) ({len(df_county_filter)} Counties)')
-funcs.print_log(file_log, f'\n\n\n## File Name Tags\n\n{dictionary.dicts['county_layer_filetype']}\n\n{df_county_layer_filetype.to_markdown(index=False)}\n', on_screen=print_on_screen)
-funcs.print_log(file_log, f'\n\n\n## IGAC Property Economic Destination\n\n{dictionary.dicts['county_layer_economic_destination_igac']}\n\n{df_county_layer_economic_destination_igac.to_markdown(index=False)}\n', on_screen=print_on_screen)
+funcs.print_log(file_log, f'\n\n\n## 2. File Name Tags\n\n{dictionary.dicts['county_layer_filetype']}\n\n{df_county_layer_filetype.to_markdown(index=False)}\n', on_screen=print_on_screen)
+funcs.print_log(file_log, f'\n\n\n## 3. IGAC Property Economic Destination\n\n{dictionary.dicts['county_layer_economic_destination_igac']}\n\n{df_county_layer_economic_destination_igac.to_markdown(index=False)}\n', on_screen=print_on_screen)
 funcs.print_log(file_log, f'\n\n#\n\n<div align="center"><img alt="rcfdtools" src="../../graph/qr-code-shp.png" width="250px"><br><sub>Share this research</sub></div><br>', on_screen=print_on_screen)
 funcs.print_log(file_log, f'\n\n<sub>{dictionary.dicts['disclaimer']}</sub>', on_screen=print_on_screen)
 funcs.print_log(file_log, f'\n\n| [:house: Home](../../../README.md)  | [:beginner: Help / Collab](https://github.com/rcfdtools/R.GISMobile/discussions) |', on_screen=print_on_screen)
