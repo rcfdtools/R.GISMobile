bf11608e5e03f4f20268b7b6cda400e917c874f4
r.cfdtools <r.cfdtools@gmail.com>
2026-09-11 16:29:41 -0500

    General update.

--- file/src/countylayer_readme.py
+++ file/src/countylayer_readme.py
@@ -92,13 +92,13 @@ for state in df_state:
     state_name = df_state_info['DeNombre'].values[0]
     df_county_filter = df_county[df_county['DeCodigo'] == state]
     funcs.print_log(file_log, f'<div align="center"><img alt="rcfdtools" src="../../graph/R.GISMobile.svg" width="250px"></div>\n\n')
-    funcs.print_log(file_log, f'# _{dictionary.dicts['study_name']} for {state} - {state_name} ({len(df_county_filter)} Counties)_ \n{dictionary.dicts['keywords']}\n\n{dictionary.dicts['study_desc']}\n\n> Check the general [DataSource & ChangeLog](Readme_Datasource.md) readme file.\n\n', on_screen=print_on_screen)
+    funcs.print_log(file_log, f'# _{dictionary.dicts['study_name']} for {state} - {state_name} ({len(df_county_filter)} Counties)_ \n{dictionary.dicts['keywords']}\n\n{dictionary.dicts['study_desc']}\n\n> Check the general [DataSource & ChangeLog](Readme_Datasource.md) readme file. \n', on_screen=print_on_screen)
+    funcs.print_log(file_log, f'> Each _CountyID_ code link contain the [Population and Public Services Demand Projections (PPSD)](https://github.com/rcfdtools/R.HydroTools/blob/main/tool/Population/file/report/Readme.md) report.\n', center_div=False, on_screen=print_on_screen)
     state_latitude = df_state_info['Latitude'].values[0]
     state_longitude = df_state_info['Longitude'].values[0]
     #funcs.print_log(file_log, f'\n# {state} - {state_name} ({len(df_county_filter)} Counties)\n')
     fig_file0a = f'{minimap_link}{country_code}_{state}_MiniMap.png'
     funcs.print_log(file_log, f'<img alt="rcfdtools" src="{fig_file0a}" width="600px"></img>', center_div=True, on_screen=print_on_screen)
-    funcs.print_log(file_log, f'> Each _CountyID_ code link contain the [Population and Public Services Demand Projections (PPSD)](https://github.com/rcfdtools/R.HydroTools/blob/main/tool/Population/file/report/Readme.md) report.\n', center_div=False, on_screen=print_on_screen)
     df_county_unique = df_county_filter['MpCodigo'].unique()
     for county in df_county_unique:
         df_cadastre_manager_info = df_cadastre_manager[df_cadastre_manager['mpcodigo'] == county]
